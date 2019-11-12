6eb0156ba7955d82525444e394183690a82869db
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-11-12 10:22:54 +0100

    Set Up Linting

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 //var chalk = require('chalk');
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting the app in Dev Mode...'))
+console.log(chalk.green('Starting the app in Dev Mode...')) // eslint-disable-line no-console
