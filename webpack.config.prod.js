f757b81fe77c0b43d410aa7155aebc4d24c01a70
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-11-19 19:50:22 +0100

    Minification with Webpack; Local/Dist Server; Toggle Mock API; Production Build npm scripts; Dynamic HTML

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from "webpack";
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
   debug: true,
@@ -18,10 +19,28 @@ export default {
       contentBase: path.resolve(__dirname,'src')
   },
   plugins: [
+    //create html file that includes reference to bundled JS
+    new HtmlWebpackPlugin ({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttribute: true,
+        keepClosingSlash: true,
+        minifyJs: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true //so we delete the script tag in html since the scrip would be injected
+    }),
+
     //Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     //Minify JS
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
   ],
   module: {
     loaders: [
